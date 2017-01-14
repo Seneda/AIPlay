c93054a667fde492dcf735e17885be699f1a52f7
Seneda <senedanassirali@gmail.com>
2017-01-14 21:29:20 +0000

    Clean up script for analysing the screen

--- Mario/lua scripts/readscreen.lua
+++ Mario/lua scripts/readscreen.lua
@@ -1,58 +1,47 @@
 function label(text, x, y, size)
+	-- Add text with a backgound box at a given location
 	aspect = 0.65
 	width = string.len(text)*size*aspect
 	gui.drawBox(x, y, x+width,y+size,0x00000000,0x80FF0000)
 	gui.drawText(x, y, text, 0xFF000000, size)
-
 end
 
 marioheight = 32
 mariowidth = 16
-mapscale = 0.4
-
-function mariobox(x, y)
-	gui.drawBox(x, y, x+15*mapscale,y+35*mapscale,0x00000000,0x8000FF00)
-end
-
-function box(text, x, y, size)
-	label(text, x, y+size/2, size)
-end
+mapscale = 0.8  -- Size of map relative to size of the screen
 
 RAM = {
 	marioX = 0xD1,
 	marioY = 0xD3,
 	layer1x = 0x1A,
-	layer1y = 0x1C
+	layer1y = 0x1C,
+	screentiles = 0x1C800,
 }
 
 MAP = {
 	x = 0,
-	width = 255*mapscale,
+	width = 270*mapscale,
 	y = 0,
-	height = 255*mapscale
+	height = 200*mapscale
 }
 
 function getPositions()
-		marioX = memory.read_s16_le(RAM.marioX)
-		marioY = memory.read_s16_le(RAM.marioY)
-		label("M", marioX, 10, 10)
-		local layer1x = memory.read_s16_le(RAM.layer1x);
-		local layer1y = memory.read_s16_le(RAM.layer1y);
-		
-		screenX = marioX-layer1x
-		screenY = marioY-layer1y
-
-		mapX = screenX/255 * MAP.width + MAP.x
-		mapY = screenY/255 * MAP.height + MAP.y
-		--mariobox(mapX, mapY)
-
+	-- Get the positions of the Mario Sprite
+	marioX = memory.read_s16_le(RAM.marioX)
+	marioY = memory.read_s16_le(RAM.marioY)
+	label("M", marioX, 10, 10)
+	local layer1x = memory.read_s16_le(RAM.layer1x);
+	local layer1y = memory.read_s16_le(RAM.layer1y);
+	
+	screenX = marioX-layer1x
+	screenY = marioY-layer1y
 end
 
 function getTile(dx, dy)
+	-- Read the memory for a map location relative to Mario's current position.
 	x = math.floor((marioX+dx+8)/16)
-	y = math.floor((marioY+dy)/16)
-	
-	return memory.readbyte(0x1C800 + math.floor(x/0x10)*0x1B0 + y*0x10 + x%0x10)	
+	y = math.floor((marioY+dy)/16)	
+	return memory.readbyte(RAM.screentiles + math.floor(x/0x10)*0x1B0 + y*0x10 + x%0x10)	
 end	
 
 function getSprites()
@@ -65,7 +54,6 @@ function getSprites()
 			sprites[#sprites+1] = {["x"]=spritex, ["y"]=spritey}
 		end
 	end		
-	
 	return sprites
 end
 
@@ -78,12 +66,13 @@ function getExtendedSprites()
 			spritey = memory.readbyte(0x1715+slot) + memory.readbyte(0x1729+slot)*256
 			extended[#extended+1] = {["x"]=spritex, ["y"]=spritey}
 		end
-	end		
-	
+	end			
 	return extended
 end
 
-BoxRadius = 6
+BoxWidth = 7
+BoxHeight = 6
+TileSize = 16
 
 function getInputs()
 	getPositions()
@@ -93,31 +82,35 @@ function getInputs()
 	
 	local inputs = {}
 	
-	for dy=-BoxRadius*16,BoxRadius*16,16 do
-		for dx=-BoxRadius*16,BoxRadius*16,16 do
-			inputs[#inputs+1] = 0
+	for j=-BoxHeight,BoxHeight,1 do
+		row = {}
+		for i=-BoxWidth,BoxWidth,1 do
+			dx = i*TileSize
+			dy = j*TileSize
+			row[#row+1] = 0
 			
 			tile = getTile(dx, dy)
 			if tile == 1 and marioY+dy < 0x1B0 then
-				inputs[#inputs] = 1
+				row[#row] = 1
 			end
 			
-			for i = 1,#sprites do
-				distx = math.abs(sprites[i]["x"] - (marioX+dx))
-				disty = math.abs(sprites[i]["y"] - (marioY+dy))
+			for n = 1,#sprites do
+				distx = math.abs(sprites[n]["x"] - (marioX+dx))
+				disty = math.abs(sprites[n]["y"] - (marioY+dy))
 				if distx <= 8 and disty <= 8 then
-					inputs[#inputs] = -1
+					row[#row] = -1
 				end
 			end
 
-			for i = 1,#extended do
-				distx = math.abs(extended[i]["x"] - (marioX+dx))
-				disty = math.abs(extended[i]["y"] - (marioY+dy))
+			for n = 1,#extended do
+				distx = math.abs(extended[n]["x"] - (marioX+dx))
+				disty = math.abs(extended[n]["y"] - (marioY+dy))
 				if distx < 8 and disty < 8 then
-					inputs[#inputs] = -1
+					row[#row] = -1
 				end
 			end
 		end
+		inputs[j+BoxHeight] = row
 	end
 	
 	--mariovx = memory.read_s8(0x7B)
@@ -126,22 +119,19 @@ function getInputs()
 	return inputs
 end
 			
-function readlocation(x, y)
-	x = math.floor(x)
-	y = math.floor(y)
-	return memory.readbyte(0x1C800 + math.floor(x/0x10)*0x1B0 + y*0x10 + x%0x10)
 
-end
+Red =   0x80FF0000
+Green = 0x8000FF00
+Blue =  0x800000FF
 
 function drawMap()
-	gui.drawBox(MAP.x, MAP.y, MAP.width, MAP.height, 0x00000000,0x80FFFFFF)
-	--mariobox(mapX, mapY)
+	drawMapBox()
 	inputs = {}
 	inputs = getInputs()
-	gridsize = 12
-	for y=0, gridsize do
-		for x=0, gridsize do
-			tile = inputs[13*y+x+1]
+	for y=0, 2*BoxHeight do
+		for x=0, 2*BoxWidth do
+			--tile = inputs[(BoxWidth*2+1)*y+x+1]
+			tile = inputs[y][x]
 			--label(tile, MAP.x + x*16*mapscale, MAP.y + y*16*mapscale, 16*mapscale)
 			if tile == 1 then
 				drawTerrain(x, y)
@@ -152,31 +142,34 @@ function drawMap()
 			
 		end
 	end
-	drawAvatar(gridsize/2, gridsize/2+1)
+	drawAvatar(BoxWidth+1, BoxHeight+1)
 end
 
-tilesize = 16
+function drawMapBox()
+	gui.drawBox(MAP.x, MAP.y, MAP.width, MAP.height, 0x00000000,0x80FFFFFF)
+end
+
+function drawMapItem(x, y, colour)
+	x1 = 2 + MAP.x + tilesize*mapscale*x
+	y1 = 2 + MAP.y + tilesize*mapscale*y
+	gui.drawBox(x1, y1, x1 + tilesize*mapscale, y1 + tilesize*mapscale, 0x00000000, colour)
+
+end
 
 function drawTerrain(x, y)
-	x1 = MAP.x + tilesize*mapscale*x
-	y1 = MAP.y + tilesize*mapscale*y
-	gui.drawBox(x1, y1, x1 + tilesize*mapscale, y1 + tilesize*mapscale, 0x00000000,0x800000FF)
+	drawMapItem(x, y, Green)
 end
 
 function drawEnemy(x, y)
-	x1 = MAP.x + tilesize*mapscale*x
-	y1 = MAP.y + tilesize*mapscale*y
-	gui.drawBox(x1, y1, x1 + tilesize*mapscale, y1 + tilesize*mapscale, 0x00000000,0x80FF0000)
+	drawMapItem(x, y, Red)
 end
 
 function drawAvatar(x, y)
-	x1 = MAP.x + tilesize*mapscale*x
-	y1 = MAP.y + tilesize*mapscale*y
-	gui.drawBox(x1, y1, x1 + tilesize*mapscale, y1 + tilesize*mapscale, 0x00000000,0x8000FF00)
+	drawMapItem(x, y, Blue)
+	drawMapItem(x, y-0.5, Blue)
 end
 
 while true do
-	getPositions()
 	drawMap()
 	label("Game Map", 10, 0, 10)
 	emu.frameadvance()
